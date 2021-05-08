4ba4df21917387abd0d3e9b731ed5cbacfc37e63
Ammar Ahmed <ammarahmed6506@gmail.com>
2021-05-09 00:34:16 +0500

    update example

--- example/App.tsx
+++ example/App.tsx
@@ -1,8 +1,45 @@
+import { createFourUIComponent, HFlex, Paragraph, VFlex } from "four-ui-kit";
+import { LiteralUnion, SimpleComponentProps } from "four-ui-kit/src/helpers/typings";
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+
+interface ButtonProps extends TouchableOpacityProps,SimpleComponentProps {
+  type?:LiteralUnion<"default" | "error" | "success">
+}
+let types: { [name: string]: ButtonProps } = {
+  default:{
+    flexDirection:"column"
+  }
+}
+const Button:React.FC<ButtonProps> = createFourUIComponent(TouchableOpacity,types);
 
 const App = () => {
-  return <SafeAreaView></SafeAreaView>;
+
+  
+
+  return <VFlex
+    type="safearea-container"
+  >
+
+    <Button/>
+
+    <VFlex
+      type="centered"
+    >
+     <HFlex
+     flex={1}
+     
+     >
+       <Paragraph>
+         Hello
+       </Paragraph>
+       <Paragraph>
+         World
+       </Paragraph>
+     </HFlex>
+    </VFlex>
+
+  </VFlex>
 };
 
 export default App;
